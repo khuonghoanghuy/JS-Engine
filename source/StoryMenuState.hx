b9f20ad7601fb4da866238dfce7c2edf9e1772fa
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2023-04-20 13:39:50 +0700

    edit story state

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -33,10 +33,18 @@ class StoryMenuState extends MusicBeatState
 	];
 	var curDifficulty:Int = 1;
 
-	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true];
+	public static var weekUnlocked:Array<Bool> = [
+		true, 
+		true, 
+		true, 
+		true, 
+		true, 
+		true, 
+		true
+	];
 
 	var weekCharacters:Array<Dynamic> = [
-		['dad', 'bf', 'gf'],
+		['', 'bf', 'gf'],
 		['dad', 'bf', 'gf'],
 		['spooky', 'bf', 'gf'],
 		['pico', 'bf', 'gf'],
