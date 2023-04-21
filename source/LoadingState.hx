b7e2174d1d1c04f964aeb0c88d66eeb7c509905e
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2023-04-21 15:58:51 +0700

    remove some stuff

--- source/LoadingState.hx
+++ source/LoadingState.hx
@@ -23,8 +23,6 @@ class LoadingState extends MusicBeatState
 	var callbacks:MultiCallback;
 	
 	var logo:FlxSprite;
-	var gfDance:FlxSprite;
-	var danceLeft = false;
 	
 	function new(target:FlxState, stopMusic:Bool)
 	{
@@ -39,17 +37,9 @@ class LoadingState extends MusicBeatState
 		logo.frames = Paths.getSparrowAtlas('logoBumpin');
 		logo.antialiasing = true;
 		logo.animation.addByPrefix('bump', 'logo bumpin', 24);
+		logo.screenCenter();
 		logo.animation.play('bump');
 		logo.updateHitbox();
-		// logoBl.screenCenter();
-		// logoBl.color = FlxColor.BLACK;
-
-		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
-		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
-		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
-		gfDance.antialiasing = true;
-		add(gfDance);
 		add(logo);
 		
 		initSongsManifest().onComplete
@@ -108,12 +98,6 @@ class LoadingState extends MusicBeatState
 		super.beatHit();
 		
 		logo.animation.play('bump');
-		danceLeft = !danceLeft;
-		
-		if (danceLeft)
-			gfDance.animation.play('danceRight');
-		else
-			gfDance.animation.play('danceLeft');
 	}
 	
 	override function update(elapsed:Float)
